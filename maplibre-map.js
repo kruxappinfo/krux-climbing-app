4e2688a61e6da00309580e3c32aa2326b4994b03
kruxappinfo <kruxappinfo@users.noreply.github.com>
2026-05-08 16:09:15 +0000

    deploy: 8a1cbc0175036b75c406c36416e00a4fbe6adf09

--- maplibre-map.js
+++ maplibre-map.js
@@ -1981,7 +1981,7 @@ const POI_SVG_MAP = {
 };
 
 // Versión de los iconos (incrementar al actualizar para forzar recarga del cache)
-const POI_ICONS_VERSION = '13';
+const POI_ICONS_VERSION = '14';
 
 // Mapa de POIs que tienen icono PNG externo disponible (los PNGs originales del diseñador)
 const POI_SVG_FILES = {
